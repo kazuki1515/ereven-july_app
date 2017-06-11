649653bb92172a2d9da803d13c28ce430bb1e645
Kazuki <wa_1023@yahoo.co.jp>
2017-06-11 13:20:07 +0000

    Initializerepository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ErevenJulyApp::Application.config.secret_key_base = 'c6817f28e18c00c851587a5c09505e319b0597864dd55f17aea2ae02925197566d0061aff90d08db5a510c992ef40a2c2f7a3736f81f99da45f67f6d05d41ccc'
